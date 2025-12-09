18c7d22b9bc47685102e1c18587d6c768b0d6644
hadin-tai <hadintai312@gmail.com>
2025-12-09 19:29:20 +0530

    Switch to Huggingface Model

--- ML Model Backend/model/llm.py
+++ ML Model Backend/model/llm.py
@@ -10,15 +10,15 @@ def get_llm():
     Returns a configured HuggingFace Endpoint LLM.
     # """
 
-    return ChatOllama(
-        model="gemma3:1b"     # 100% works on 4GB VRAM
-    )
-
-    # return ChatHuggingFace(
-    #     llm=HuggingFaceEndpoint(
-    #         repo_id="google/gemma-2-2b-it",
-    #         task="conversational",
-    #         max_new_tokens=512,
-    #         huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
-    #     )
+    # return ChatOllama(
+    #     model="gemma3:1b"     # 100% works on 4GB VRAM
     # )
+
+    return ChatHuggingFace(
+        llm=HuggingFaceEndpoint(
+            repo_id="google/gemma-2-2b-it",
+            task="conversational",
+            max_new_tokens=512,
+            huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+        )
+    )
