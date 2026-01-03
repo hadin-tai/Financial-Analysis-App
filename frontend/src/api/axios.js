930cb3f9c78533a8048998014eaf691118ab6f66
hadin-tai <hadintai312@gmail.com>
2026-01-03 15:27:22 +0530

    .

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
+  ? `${import.meta.env.VITE_BACKEND_URL}/api`
+  : "http://localhost:5000/api";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ Your backend URL
+  // baseURL: 'http://localhost:5000/api', // ✅ Your backend URL
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 instance.interceptors.request.use(
   (config) => {
